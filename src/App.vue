ebdfeb02134c6d41c604b0377b354bcfcb8614d9
digitalsamsquanch <digital.samsquanch@protonmail.com>
2022-01-16 22:57:56 -0500

    working through the VideoList enumeration

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container"><SearchBar @termChange="onTermChange"></SearchBar>
+  <VideoList :videos="videos"></VideoList>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import configData from './config.json'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    SearchBar,
+    VideoList
+  },
+  data() {
+    return {videos: [] }
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      axios.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          key: configData.API_KEY,
+          type: 'video',
+          part: 'snippet',
+          q: searchTerm,
+        },
+      }).then(response => {
+        this.videos = response.data.items;
+      });
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/VideoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul class="list-group">
+      <VideoListItem 
+        v-for="video in videos"
+       :video="video" 
+       :key="video.etag">
+       
+       </VideoListItem>
+       {{ videos }}
+  </ul>
+</template>
+
+<script>
+import VideoListItem from './VideoListItem.vue'
+export default {
+    name: 'VideoList',
+    components: {
+        VideoListItem
+    },
+    props: ['videos']
+}
+</script>
+
+<style>
+
+</style>
